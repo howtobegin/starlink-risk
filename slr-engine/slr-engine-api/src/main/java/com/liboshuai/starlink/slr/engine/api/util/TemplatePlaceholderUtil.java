0776eccefd67cf161aa415d5235037f87f86baaa
BoShuai Li <liboshuai01@gmail.com>
2024-12-16 16:04:50 +0800

    feat(slr-engine): 预警信息模板替换工具类

---
+++ slr-engine/slr-engine-api/src/main/java/com/liboshuai/starlink/slr/engine/api/util/TemplatePlaceholderUtil.java
@@ -0,0 +1,99 @@
+package com.liboshuai.starlink.slr.engine.api.util;
+
+import org.apache.commons.text.StringSubstitutor;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 字符串消息模板替换 工具类
+ */
+public class TemplatePlaceholderUtil {
+
+    private static final String SEPARATOR = ".";
+
+    /**
+     * 替换模板中的占位符
+     *
+     * @param template 消息模板，支持占位符如 ${KafkaPojo.channel}
+     * @param objects  需要绑定的对象，可以是多个对象
+     * @return 替换占位符后的字符串
+     */
+    public static String replacePlaceholders(String template, Object... objects) {
+        Map<String, String> placeholderMap = new HashMap<>();
+
+        for (Object obj : objects) {
+            if (obj != null) {
+                String objectPrefix = obj.getClass().getSimpleName(); // 类名作为前缀
+                populatePlaceholderMap(objectPrefix, obj, placeholderMap);
+            }
+        }
+
+        // 使用 StringSubstitutor 进行占位符替换
+        StringSubstitutor substitutor = new StringSubstitutor(placeholderMap);
+        return substitutor.replace(template);
+    }
+
+    /**
+     * 展开对象的所有字段，构造占位符对应的值
+     *
+     * @param parentPrefix   当前字段的路径前缀
+     * @param obj            当前对象
+     * @param placeholderMap 用于存储占位符和实际值的映射表
+     */
+    private static void populatePlaceholderMap(String parentPrefix, Object obj, Map<String, String> placeholderMap) {
+        if (obj == null) {
+            return;
+        }
+
+        // 如果是 Map 类型，递归处理其键值对
+        if (obj instanceof Map) {
+            @SuppressWarnings("unchecked")
+            Map<String, Object> map = (Map<String, Object>) obj;
+            for (Map.Entry<String, Object> entry : map.entrySet()) {
+                String mapKey = parentPrefix + SEPARATOR + entry.getKey();
+                populatePlaceholderMap(mapKey, entry.getValue(), placeholderMap);
+            }
+        }
+        // 如果是 List 类型，递归处理每个元素
+        else if (obj instanceof List) {
+            List<?> list = (List<?>) obj;
+            for (int i = 0; i < list.size(); i++) {
+                String listKey = parentPrefix + SEPARATOR + i;
+                populatePlaceholderMap(listKey, list.get(i), placeholderMap);
+            }
+        }
+        // 如果是基本类型、字符串或其他简单值，直接放入占位符映射
+        else if (isPrimitiveOrWrapper(obj.getClass()) || obj instanceof String) {
+            placeholderMap.put(parentPrefix, obj.toString());
+        }
+        // 如果是复杂对象，递归展开其字段
+        else {
+            Field[] fields = obj.getClass().getDeclaredFields();
+            for (Field field : fields) {
+                field.setAccessible(true); // 允许访问私有字段
+                try {
+                    Object value = field.get(obj); // 获取字段值
+                    String fieldKey = parentPrefix + SEPARATOR + field.getName();
+                    populatePlaceholderMap(fieldKey, value, placeholderMap);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Error accessing field: " + field.getName(), e);
+                }
+            }
+        }
+    }
+
+    /**
+     * 判断一个类型是否为基本类型或其包装类型
+     *
+     * @param clazz 类型
+     * @return 是否为基本类型或包装类型
+     */
+    private static boolean isPrimitiveOrWrapper(Class<?> clazz) {
+        return clazz.isPrimitive() || clazz == Boolean.class || clazz == Byte.class ||
+                clazz == Character.class || clazz == Short.class || clazz == Integer.class ||
+                clazz == Long.class || clazz == Float.class || clazz == Double.class;
+    }
+}
---
+++ slr-engine/slr-engine-biz/src/test/java/com/liboshuai/starlink/TemplatePlaceholderUtilTest.java
@@ -0,0 +1,137 @@
+package com.liboshuai.starlink;
+
+import com.liboshuai.starlink.slr.engine.api.dto.EventKafkaDTO;
+import com.liboshuai.starlink.slr.engine.api.dto.RuleConditionDTO;
+import com.liboshuai.starlink.slr.engine.api.dto.RuleInfoDTO;
+import com.liboshuai.starlink.slr.engine.api.util.TemplatePlaceholderUtil;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TemplatePlaceholderUtilTest {
+
+    @Test
+    public void testReplacePlaceholders() {
+        KafkaPojo kafkaPojo = KafkaPojo.builder()
+                .channel("game")
+                .userName("wdy")
+                .userId("000001")
+                .build();
+
+        MysqlPojo mysqlPojo = MysqlPojo.builder()
+                .ruleName("游戏高频抽奖")
+                .ruleCode("game_lottery")
+                .rulePojo(RulePojo.builder().ruleType("1").ruleValue("10").build())
+                .build();
+
+        String messageTemplate = "${KafkaPojo.channel}渠道的${KafkaPojo.userName}(${KafkaPojo.userId})用户触发了" +
+                "${MysqlPojo.ruleName}(${MysqlPojo.ruleCode})规则，其中规则类型为${MysqlPojo.rulePojo.ruleType}" +
+                "、规则值为${MysqlPojo.rulePojo.ruleValue}。请尽快处理!";
+
+        String messageTemplate2 = "[异常高频抽奖]${EventKafkaDTO.attribute.bankName}：${EventKafkaDTO.attribute.campaignName}" +
+                "(${EventKafkaDTO.attribute.campaignId})中游戏用户(${EventKafkaDTO.attribute.userId})" +
+                "最近${RuleInfoDTO.ruleConditionGroup.0.windowSize}内抽奖数量为${xxx}，" +
+                "超过${RuleInfoDTO.ruleConditionGroup.0.eventThreshold}次，请您及时查看原因！";
+
+        // 使用工具类替换模板中的占位符
+        String result = TemplatePlaceholderUtil.replacePlaceholders(messageTemplate, kafkaPojo, mysqlPojo);
+
+        System.out.println("Result: " + result);
+    }
+
+    @Test
+    public void testReplacePlaceholders2() {
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put("userId", "userId01");
+        attributes.put("bankName", "bankName01");
+        attributes.put("campaignId", "campaignId01");
+        attributes.put("campaignName", "campaignName01");
+
+        EventKafkaDTO eventKafkaDTO = EventKafkaDTO.builder()
+                .channel("game")
+                .keyCode("keyCode1")
+                .keyValue("keyValue1")
+                .eventCode("eventCode1")
+                .eventValue("eventValue1")
+                .attribute(attributes)
+                .timestamp(System.currentTimeMillis())
+                .build();
+
+        List<RuleConditionDTO> ruleConditionDTOS = new ArrayList<>();
+        ruleConditionDTOS.add(RuleConditionDTO.builder()
+                .ruleCode("ruleCode1")
+                .eventCode("eventCode1")
+                .eventThreshold(10L)
+                .conditionType(1)
+                .windowSize(20L)
+                .isCrossHistory(true)
+                .build());
+        ruleConditionDTOS.add(RuleConditionDTO.builder()
+                .ruleCode("ruleCode2")
+                .eventCode("eventCode2")
+                .eventThreshold(20L)
+                .conditionType(1)
+                .windowSize(30L)
+                .isCrossHistory(true)
+                .build());
+        RuleInfoDTO ruleInfoDTO = RuleInfoDTO.builder()
+                .channel("game")
+                .ruleCode("ruleCode1")
+                .ruleName("ruleName1")
+                .ruleDesc("ruleDesc1")
+                .modelCode("modelCode1")
+                .combinedConditionOperator(0)
+                .warnMessage("warnMessage1")
+                .warnInterval(10L)
+                .status(1)
+                .ruleConditionGroup(ruleConditionDTOS)
+                .ruleModelGroovyCode("xxx")
+                .build();
+
+        String messageTemplate = "[异常高频抽奖]${EventKafkaDTO.attribute.bankName}：" +
+                "${EventKafkaDTO.attribute.campaignName}(${EventKafkaDTO.attribute.campaignId})" +
+                "中游戏用户(${EventKafkaDTO.attribute.userId})" +
+                "最近${RuleInfoDTO.ruleConditionGroup.1.windowSize}内抽奖数量为${xxx}，" +
+                "超过${RuleInfoDTO.ruleConditionGroup.1.eventThreshold}次，请您及时查看原因！";
+
+        // 使用工具类替换模板中的占位符
+        String result = TemplatePlaceholderUtil.replacePlaceholders(messageTemplate, eventKafkaDTO, ruleInfoDTO);
+
+        System.out.println("Result: " + result);
+    }
+}
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+class KafkaPojo {
+    private String channel;
+    private String userName;
+    private String userId;
+}
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+class MysqlPojo {
+    private String ruleName;
+    private String ruleCode;
+    private RulePojo rulePojo;
+}
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+class RulePojo {
+    private String ruleValue;
+    private String ruleType;
+}
