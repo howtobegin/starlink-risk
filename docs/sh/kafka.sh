21d36045af7997b1070848bd4d1b30495dd42d5e
BoShuai Li <liboshuai01@gmail.com>
2024-12-24 10:18:27 +0800

    docs(kafka): 更新 Kafka脚本中的主机地址
    -将固定的 Windows 主机地址替换为动态的 Kafka 主机 IP
    - 使用 #{kafka主机IP}占位符，以便在不同环境中灵活配置 Kafka 服务器地址

--- docs/sh/kafka.sh
+++ docs/sh/kafka.sh
@@ -1,22 +1,22 @@
 # 创建 slr_event topic，用于接收业务方的事件数据
 kafka-topics.sh --create \
---bootstrap-server windows:9096,windows:9097,windows:9098 \
+--bootstrap-server #{kafka主机IP}:9096,#{kafka主机IP}:9097,#{kafka主机IP}:9098 \
 --replication-factor 3 \
 --partitions 6 \
 --topic slr_event
 
 # 创建 slr_alert topic，用于接收 flink 生成的告警数据
 kafka-topics.sh --create \
---bootstrap-server windows:9096,windows:9097,windows:9098 \
+--bootstrap-server #{kafka主机IP}:9096,#{kafka主机IP}:9097,#{kafka主机IP}:9098 \
 --replication-factor 3 \
 --partitions 6 \
 --topic slr_alert
 
 # 创建 top 列表
-kafka-topics.sh --list --bootstrap-server windows:9096,windows:9097,windows:9098
+kafka-topics.sh --list --bootstrap-server #{kafka主机IP}:9096,#{kafka主机IP}:9097,#{kafka主机IP}:9098
 
 # 向 slr_event topic 发送单条数据，模拟业务方数据生产
-kafka-console-producer.sh --broker-list windows:9096,windows:9097,windows:9098 --topic slr_event
+kafka-console-producer.sh --broker-list #{kafka主机IP}:9096,#{kafka主机IP}:9097,#{kafka主机IP}:9098 --topic slr_event
 
 # 消费 slr_alert top 数据，查看flink 生成的告警数据
-kafka-console-consumer.sh --bootstrap-server windows:9096,windows:9097,windows:9098 --topic slr_alert
+kafka-console-consumer.sh --bootstrap-server #{kafka主机IP}:9096,#{kafka主机IP}:9097,#{kafka主机IP}:9098 --topic slr_alert
