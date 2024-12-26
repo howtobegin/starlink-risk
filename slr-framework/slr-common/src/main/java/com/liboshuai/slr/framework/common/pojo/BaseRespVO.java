84998e24b04d8fa75ba76ee7c777d8c39ca71e2e
BoShuai Li <liboshuai01@gmail.com>
2024-12-27 00:33:19 +0800

    refactor(admin): 优化日期格式化处理
    - 在 RuleCondRespVO 和 BaseRespVO 类中添加 @DateTimeFormat 和 @JsonFormat 注解
    - 统一日期格式为 "yyyy-MM-dd HH:mm:ss"
    - 设置时区为 GMT+8
    - 改进日期相关字段的序列化和反序列化处理

--- slr-framework/slr-common/src/main/java/com/liboshuai/slr/framework/common/pojo/BaseRespVO.java
+++ slr-framework/slr-common/src/main/java/com/liboshuai/slr/framework/common/pojo/BaseRespVO.java
@@ -1,7 +1,9 @@
 package com.liboshuai.slr.framework.common.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -16,9 +18,13 @@ public class BaseRespVO implements Serializable {
     @Schema(description = "主键ID", example = "1")
     private Long id;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @Schema(description = "创建时间", example = "2025-01-01")
     private LocalDateTime createTime;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @Schema(description = "最后更新时间", example = "2025-01-01")
     private LocalDateTime updateTime;
 
