1b23c72483b8374838ba29625517463b11f3af11
BoShuai Li <liboshuai01@gmail.com>
2024-12-26 11:26:28 +0800

    feat(riskRule): 优化规则信息查询接口
    - 在 RuleInfoRespVO 类中添加 RuleModelRespVO 类型的字段
    - 在 RuleModelMapper 中新增 selectListByModelCode 方法
    - 修改 RuleInfoServiceImpl 中的 list 方法，增加模型信息的查询和设置

--- slr-admin/slr-admin-biz/src/main/java/com/liboshuai/slr/module/admin/dal/mysql/riskRule/RuleModelMapper.java
+++ slr-admin/slr-admin-biz/src/main/java/com/liboshuai/slr/module/admin/dal/mysql/riskRule/RuleModelMapper.java
@@ -1,9 +1,17 @@
 package com.liboshuai.slr.module.admin.dal.mysql.riskRule;
 
 import com.liboshuai.slr.framework.mybatis.core.mapper.BaseMapperX;
+import com.liboshuai.slr.framework.mybatis.core.query.LambdaQueryWrapperX;
 import com.liboshuai.slr.module.admin.dal.dataobject.riskRule.RuleModelDO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface RuleModelMapper extends BaseMapperX<RuleModelDO> {
+
+    default List<RuleModelDO> selectListByModelCode(List<String> modelCodeList) {
+        return selectList(new LambdaQueryWrapperX<RuleModelDO>()
+                .in(RuleModelDO::getModelCode, modelCodeList));
+    }
 }
--- slr-admin/slr-admin-biz/src/main/java/com/liboshuai/slr/module/admin/service/riskRule/impl/RuleInfoServiceImpl.java
+++ slr-admin/slr-admin-biz/src/main/java/com/liboshuai/slr/module/admin/service/riskRule/impl/RuleInfoServiceImpl.java
@@ -4,21 +4,61 @@ import com.liboshuai.slr.framework.common.pojo.PageResult;
 import com.liboshuai.slr.framework.common.util.object.BeanUtils;
 import com.liboshuai.slr.module.admin.controller.riskRule.vo.RuleInfoReqVO;
 import com.liboshuai.slr.module.admin.controller.riskRule.vo.RuleInfoRespVO;
+import com.liboshuai.slr.module.admin.controller.riskRule.vo.RuleModelRespVO;
 import com.liboshuai.slr.module.admin.dal.dataobject.riskRule.RuleInfoDO;
+import com.liboshuai.slr.module.admin.dal.dataobject.riskRule.RuleModelDO;
 import com.liboshuai.slr.module.admin.dal.mysql.riskRule.RuleInfoMapper;
+import com.liboshuai.slr.module.admin.dal.mysql.riskRule.RuleModelMapper;
 import com.liboshuai.slr.module.admin.service.riskRule.RuleInfoService;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class RuleInfoServiceImpl implements RuleInfoService {
     @Resource
     private RuleInfoMapper ruleInfoMapper;
+    @Resource
+    private RuleModelMapper ruleModelMapper;
 
     @Override
     public PageResult<RuleInfoRespVO> list(RuleInfoReqVO ruleInfoReqVO) {
         PageResult<RuleInfoDO> ruleInfoEntityPageResult = ruleInfoMapper.selectPage(ruleInfoReqVO);
-        return BeanUtils.toBean(ruleInfoEntityPageResult, RuleInfoRespVO.class);
+        PageResult<RuleInfoRespVO> ruleInfoRespVOPageResult = BeanUtils.toBean(ruleInfoEntityPageResult, RuleInfoRespVO.class);
+        List<RuleInfoRespVO> ruleInfoRespVOList = ruleInfoRespVOPageResult.getList();
+        if (CollectionUtils.isEmpty(ruleInfoRespVOList)) {
+            return ruleInfoRespVOPageResult;
+        }
+        // 设置模型信息
+        PageResult<RuleInfoRespVO> respVOPageResult = setRuleModelRespVO(ruleInfoRespVOList, ruleInfoRespVOPageResult);
+        if (respVOPageResult != null) return respVOPageResult;
+        // 设置条件组
+
+        return ruleInfoRespVOPageResult;
+    }
+
+    private PageResult<RuleInfoRespVO> setRuleModelRespVO(List<RuleInfoRespVO> ruleInfoRespVOList, PageResult<RuleInfoRespVO> ruleInfoRespVOPageResult) {
+        List<String> modelCodeList = ruleInfoRespVOList.stream().map(RuleInfoRespVO::getModelCode).collect(Collectors.toList());
+        List<RuleModelDO> ruleModelDOList = ruleModelMapper.selectListByModelCode(modelCodeList);
+        if (CollectionUtils.isEmpty(ruleModelDOList)) {
+            return ruleInfoRespVOPageResult;
+        }
+        List<RuleModelRespVO> ruleModelRespVOList = BeanUtils.toBean(ruleModelDOList, RuleModelRespVO.class);
+        Map<String, List<RuleModelRespVO>> ruleCodeAndRuleModelRespVoMap = ruleModelRespVOList.stream()
+                .collect(Collectors.groupingBy(RuleModelRespVO::getModelCode));
+        List<RuleInfoRespVO> ruleInfoRespVOS = ruleInfoRespVOList.stream().map(ruleInfoRespVO -> {
+            List<RuleModelRespVO> ruleModelRespVOS = ruleCodeAndRuleModelRespVoMap.get(ruleInfoRespVO.getModelCode());
+            if (CollectionUtils.isEmpty(ruleModelRespVOS)) {
+                return ruleInfoRespVO;
+            }
+            ruleInfoRespVO.setRuleModelRespVO(ruleModelRespVOS.get(0));
+            return ruleInfoRespVO;
+        }).collect(Collectors.toList());
+        ruleInfoRespVOPageResult.setList(ruleInfoRespVOS);
+        return null;
     }
 }
