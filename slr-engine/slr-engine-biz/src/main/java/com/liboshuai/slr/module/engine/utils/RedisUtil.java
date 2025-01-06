59b98a59d21f410d2196fac756eb084a713d741d
BoShuai Li <liboshuai01@gmail.com>
2025-01-06 09:57:33 +0800

    Revert "refactor(engine): 优化模型一的 Redis 使用"
    This reverts commit 7a32f3ae85e37fc2021706d69262aa770897e9e2.

--- slr-engine/slr-engine-biz/src/main/java/com/liboshuai/slr/module/engine/utils/RedisUtil.java
+++ slr-engine/slr-engine-biz/src/main/java/com/liboshuai/slr/module/engine/utils/RedisUtil.java
@@ -49,13 +49,10 @@ public class RedisUtil {
         poolConfig.setTestWhileIdle(true);
         // 创建JedisCluster
         if (!StringUtils.isEmpty(password)) {
-//            jedisCluster = new JedisCluster(jedisClusterNodes, connectionTimeout, soTimeout, maxAttempts, password, poolConfig);
-            // FIXME: 暂时取消redis加载连接，降低系统复杂度
-            jedisCluster = null;
+            jedisCluster = new JedisCluster(jedisClusterNodes, connectionTimeout, soTimeout, maxAttempts, password, poolConfig);
+
         } else {
-//            jedisCluster = new JedisCluster(jedisClusterNodes, connectionTimeout, soTimeout, maxAttempts, poolConfig);
-            // FIXME: 暂时取消redis加载连接，降低系统复杂度
-            jedisCluster = null;
+            jedisCluster = new JedisCluster(jedisClusterNodes, connectionTimeout, soTimeout, maxAttempts, poolConfig);
         }
     }
 
