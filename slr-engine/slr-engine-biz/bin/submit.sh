7adfb0dc2814f0e583ef3fcfda5ecf7fff9ae7d3
BoShuai Li <liboshuai01@gmail.com>
2025-01-09 16:48:16 +0800

    feat(slr-engine): 添加 submit.sh 脚本
    - 新增 submit.sh 脚本文件，

---
+++ slr-engine/slr-engine-biz/bin/submit.sh
@@ -0,0 +1,4 @@
+/home/lbs/software/flink/bin/flink run \
+  -m one:8081 \
+  -c com.liboshuai.slr.module.engine.EngineApplication \
+  /home/lbs/project/starlink-risk/slr-engine/slr-engine-biz/target/slr-engine-biz-1.0.jar
