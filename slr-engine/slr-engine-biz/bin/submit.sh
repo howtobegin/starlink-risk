2306844bfd2e488b6f4fec2c7856da61480a940c
BoShuai Li <liboshuai01@gmail.com>
2025-01-10 16:51:38 +0800

    perf(slr-engine): 优化 Flink 作业的 RocksDB 后端配置
    - 增加了 RocksDB 后端的缓存大小、写入缓冲区大小等参数
    - 启用了增量检查点和指标监控
    - 调整了线程数量以提高性能

--- slr-engine/slr-engine-biz/bin/submit.sh
+++ slr-engine/slr-engine-biz/bin/submit.sh
@@ -1,4 +1,10 @@
 /home/lbs/software/flink/bin/flink run \
   -m one:8081 \
   -c com.liboshuai.slr.module.engine.EngineApplication \
+  -Dstate.backend.rocksdb.block.cache-size=512mb \
+  -Dstate.backend.rocksdb.writebuffer.size=128mb \
+  -Dstate.backend.rocksdb.writebuffer.count=4 \
+  -Dstate.backend.rocksdb.thread.num=4 \
+  -Dstate.backend.rocksdb.incremental-checkpointing=true \
+  -Dstate.backend.rocksdb.metrics.enable=true \
   /home/lbs/project/starlink-risk/slr-engine/slr-engine-biz/target/slr-engine-biz-1.0.jar
