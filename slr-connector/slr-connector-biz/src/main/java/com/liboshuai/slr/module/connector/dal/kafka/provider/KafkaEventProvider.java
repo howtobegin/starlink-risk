548a4bf132bed249eece86ce7cb73eddb394749e
BoShuai Li <liboshuai01@gmail.com>
2025-01-13 10:05:03 +0800

    feat(kafka): 为 Kafka消息添加键值
    - 在发送 Kafka 消息时，增加了生成消息键值的逻辑
    - 键值由 channel、targetField 和 targetValue 组成，使用下划线连接
    - 这种做法可以避免键值冲突，提高消息处理的准确性

--- slr-connector/slr-connector-biz/src/main/java/com/liboshuai/slr/module/connector/dal/kafka/provider/KafkaEventProvider.java
+++ slr-connector/slr-connector-biz/src/main/java/com/liboshuai/slr/module/connector/dal/kafka/provider/KafkaEventProvider.java
@@ -1,5 +1,6 @@
 package com.liboshuai.slr.module.connector.dal.kafka.provider;
 
+import com.liboshuai.slr.framework.common.constants.DefaultConstants;
 import com.liboshuai.slr.module.engine.dto.KafkaEventDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -27,6 +28,19 @@ public class KafkaEventProvider {
         if (CollectionUtils.isEmpty(kafkaEventDTOList)) {
             return;
         }
-        kafkaEventDTOList.forEach(eventUploadDTO -> kafkaTemplate.send(providerTopic, eventUploadDTO));
+        kafkaEventDTOList.forEach(eventUploadDTO -> kafkaTemplate.send(
+                providerTopic, generateKey(eventUploadDTO), eventUploadDTO)
+        );
+    }
+
+    /**
+     * 根据 channel、targetField 和 targetValue 生成消息的 key
+     */
+    private String generateKey(KafkaEventDTO eventUploadDTO) {
+        String channel = eventUploadDTO.getChannel();
+        String targetField = eventUploadDTO.getTargetField();
+        String targetValue = eventUploadDTO.getTargetValue();
+        // 使用下划线连接，避免键值冲突
+        return String.join(DefaultConstants.UNDERSCORE, channel, targetField, targetValue);
     }
 }
