a551fa2136b66f851c5e256d64c83c49fe26ed1b
BoShuai Li <liboshuai01@gmail.com>
2025-01-17 11:42:27 +0800

    refactor(slr-engine): 移除 submit.sh 中的性能分析参数
    移除了 submit.sh 脚本中不再需要的性能分析参数 -Drest.flamegraph.enabled=true，减少了潜在的性能问题。

--- slr-engine/slr-engine-biz/bin/submit.sh
+++ slr-engine/slr-engine-biz/bin/submit.sh
@@ -1,5 +1,4 @@
 /home/lbs/software/flink/bin/flink run \
   -m one:8081 \
   -c com.liboshuai.slr.module.engine.EngineApplication \
-  -Drest.flamegraph.enabled=true \
   /home/lbs/project/starlink-risk/slr-engine/slr-engine-biz/target/slr-engine-biz-1.0.jar
