a87d7930daa40e6470fe297d251d965abf274879
BoShuai Li <liboshuai01@gmail.com>
2025-01-17 11:52:36 +0800

    chore(slr-engine): 删除 slr-engine-biz 模块的 submit.sh 脚本
    - 移除了 slr-engine-biz/bin/submit.sh 文件
    - 该脚本可能已不再使用或已被其他方法替代

--- slr-engine/slr-engine-biz/bin/submit.sh
+++
@@ -1,4 +0,0 @@
-/home/lbs/software/flink/bin/flink run \
-  -m one:8081 \
-  -c com.liboshuai.slr.module.engine.EngineApplication \
-  /home/lbs/project/starlink-risk/slr-engine/slr-engine-biz/target/slr-engine-biz-1.0.jar
