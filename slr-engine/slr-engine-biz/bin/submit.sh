743fdda5018658807974d2a3e374ecdd316e6356
BoShuai Li <liboshuai01@gmail.com>
2025-01-17 11:07:36 +0800

    Reapply "perf(slr-engine): 启用 Flink REST API火焰图"
    This reverts commit 6f8e26a143b924138d29160f86e7c652c9d2e6b0.

--- slr-engine/slr-engine-biz/bin/submit.sh
+++ slr-engine/slr-engine-biz/bin/submit.sh
@@ -1,4 +1,5 @@
 /home/lbs/software/flink/bin/flink run \
   -m one:8081 \
   -c com.liboshuai.slr.module.engine.EngineApplication \
+  -Drest.flamegraph.enabled=true \
   /home/lbs/project/starlink-risk/slr-engine/slr-engine-biz/target/slr-engine-biz-1.0.jar
