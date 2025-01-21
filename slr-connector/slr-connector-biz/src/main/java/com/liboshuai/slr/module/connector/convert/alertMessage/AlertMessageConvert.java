2910926c36203934a4ef425b24d023cac4540859
BoShuai Li <liboshuai01@gmail.com>
2025-01-21 18:11:21 +0800

    refactor(slr): 移除 LocalDateTimeMapper 类
    移除了 slr-common 模块中的 LocalDateTimeMapper 类，并在 AlertMessageConvert 接口中添加了处理 LocalDateTime 和 String 之间转换的方法。这样可以减少类间的依赖，简化代码结构。

--- slr-connector/slr-connector-biz/src/main/java/com/liboshuai/slr/module/connector/convert/alertMessage/AlertMessageConvert.java
+++ slr-connector/slr-connector-biz/src/main/java/com/liboshuai/slr/module/connector/convert/alertMessage/AlertMessageConvert.java
@@ -1,17 +1,37 @@
 package com.liboshuai.slr.module.connector.convert.alertMessage;
 
-import com.liboshuai.slr.framework.common.convert.LocalDateTimeMapper;
+import com.liboshuai.slr.framework.common.util.date.LocalDateTimeUtils;
 import com.liboshuai.slr.module.connector.api.alertMessage.dto.AlertMessageApiDTO;
 import com.liboshuai.slr.module.connector.dal.dataobject.alertMessage.AlertMessageDO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.springframework.util.StringUtils;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = LocalDateTimeMapper.class)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface AlertMessageConvert {
 
+    @Mapping(source = "alertTime", target = "alertTime", qualifiedByName = "string2LocalDateTime")
     List<AlertMessageDO> batchConvertDto2Mongo(List<AlertMessageApiDTO> alertMessageApiDTOList);
 
+    @Mapping(source = "alertTime", target = "alertTime", qualifiedByName = "localDateTime2String")
     List<AlertMessageApiDTO> batchConvertMongo2Dto(List<AlertMessageDO> alertMessageDOList);
+
+    default LocalDateTime string2LocalDateTime(String string) {
+        if (!StringUtils.hasText(string)) {
+            return null;
+        }
+        return LocalDateTimeUtils.convertStr2LocalDateTime(string);
+    }
+
+    default String localDateTime2String(LocalDateTime localDateTime) {
+        if (Objects.isNull(localDateTime)) {
+            return null;
+        }
+        return LocalDateTimeUtils.convertLocalDateTime2Str(localDateTime);
+    }
 }
