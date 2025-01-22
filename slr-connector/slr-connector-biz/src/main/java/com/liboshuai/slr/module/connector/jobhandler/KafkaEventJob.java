42f895cb75121f04a35de6eed9925decba8037e3
BoShuai Li <liboshuai01@gmail.com>
2025-01-23 06:24:05 +0800

    feat(connector): 为 KafkaEventJob 类添加性能监控注解
    - 在 KafkaEventJob 类的 deleteOldEventFromMongo 方法上添加了 @TakeTime 注解
    -此修改将有助于监控该方法的执行时间，为性能优化提供数据支持

--- slr-connector/slr-connector-biz/src/main/java/com/liboshuai/slr/module/connector/jobhandler/KafkaEventJob.java
+++ slr-connector/slr-connector-biz/src/main/java/com/liboshuai/slr/module/connector/jobhandler/KafkaEventJob.java
@@ -1,5 +1,6 @@
 package com.liboshuai.slr.module.connector.jobhandler;
 
+import com.liboshuai.slr.framework.takeTime.core.aop.TakeTime;
 import com.liboshuai.slr.module.connector.service.kafkaEvent.KafkaEventService;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ public class KafkaEventJob {
     /**
      * 删除mongo中过期事件数据
      */
+    @TakeTime
     @XxlJob("deleteOldEventFromMongo")
     public void deleteOldEventFromMongo() throws Exception {
         kafkaEventService.deleteOldEventFromMongo();
