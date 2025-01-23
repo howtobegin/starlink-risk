56cea1e81dbb994b8a0e568b59f682aa1337de1a
BoShuai Li <liboshuai01@gmail.com>
2025-01-23 14:57:56 +0800

    refactor(slr-engine): 更新配置和文档以适应新架构
    - 修改 Kafka、Doris 和 MySQL配置以适应新架构
    - 更新 Doris SQL 文档以反映表结构变化
    - 移除不必要的规则历史表
    - 调整测试环境配置

--- docs/sh/kafka.sh
+++ docs/sh/kafka.sh
@@ -1,16 +1,16 @@
-# 创建 slr_event topic，用于接收业务方的事件数据
+# 创建 starlink_risk_event topic，用于接收业务方的事件数据
 kafka-topics.sh --create \
 --bootstrap-server ${宿主机IP}:9096,${宿主机IP}:9097,${宿主机IP}:9098 \
 --replication-factor 3 \
 --partitions 6 \
---topic slr_event
+--topic starlink_risk_event
 
-# 创建 slr_alert topic，用于接收 flink 生成的告警数据
+# 创建 starlink_risk_alert topic，用于接收 flink 生成的告警数据
 kafka-topics.sh --create \
 --bootstrap-server ${宿主机IP}:9096,${宿主机IP}:9097,${宿主机IP}:9098 \
 --replication-factor 3 \
 --partitions 6 \
---topic slr_alert
+--topic starlink_risk_alert
 
 # 创建 top 列表
 kafka-topics.sh --list --bootstrap-server ${宿主机IP}:9096,${宿主机IP}:9097,${宿主机IP}:9098
