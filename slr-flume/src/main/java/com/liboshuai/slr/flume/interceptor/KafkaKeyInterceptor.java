b7f3cbc34dc1f6522a0aff16a9ddf0eea5afedd1
BoShuai Li <liboshuai01@gmail.com>
2025-02-16 17:32:45 +0800

    feat(slr): 添加 Flume项目并更新相关配置
    - 新增 slr-flume 模块，实现 Kafka key 拦截器
    - 更新 slr-dependencies/pom.xml，添加 flume版本依赖
    - 新增 slr-flume/pom.xml，定义 flume 项目依赖
    - 修改 flume 配置文件，使用 KafkaChannel 替代内存通道
    - 更新根 pom.xml，添加 slr-flume 模块引用

---
+++ slr-flume/src/main/java/com/liboshuai/slr/flume/interceptor/KafkaKeyInterceptor.java
@@ -0,0 +1,68 @@
+package com.liboshuai.slr.flume.interceptor;
+
+import com.liboshuai.slr.engine.api.dto.NginxEventDTO;
+import com.liboshuai.slr.framework.common.constants.RedisKeyConstants;
+import com.liboshuai.slr.framework.common.util.json.JsonUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flume.Event;
+import org.apache.flume.interceptor.Interceptor;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * 获取 Kafka key 拦截器
+ */
+@Slf4j
+public class KafkaKeyInterceptor implements Interceptor {
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public Event intercept(Event event) {
+        if (event == null) {
+            return null;
+        }
+
+        try {
+            // 将事件的 body 从 byte 转换为 String
+            String eventData = new String(event.getBody(), StandardCharsets.UTF_8);
+
+            // 使用 Jackson 解析 JSON
+            NginxEventDTO nginxEventDTO = JsonUtils.parseObject(eventData, NginxEventDTO.class);
+            if (Objects.isNull(nginxEventDTO)) {
+                return null;
+            }
+
+            // 获取需要的字段
+            String channel = nginxEventDTO.getChannel();
+            String targetField = nginxEventDTO.getTargetField();
+            String targetValue = nginxEventDTO.getTargetValue();
+
+            // 构建 key
+            String kafkaKey = channel + RedisKeyConstants.REDIS_KEY_SPLIT + targetField + RedisKeyConstants.REDIS_KEY_SPLIT + targetValue;
+
+            // 将生成的 key 设置为 event 的 header
+            event.getHeaders().put("kafkaKey", kafkaKey);
+
+            return event;
+        } catch (Exception e) {
+            log.error("处理事件数据错误: ", e);
+            return null;
+        }
+    }
+
+    @Override
+    public List<Event> intercept(List<Event> list) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
